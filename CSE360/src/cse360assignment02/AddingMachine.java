a3343424018cfad24f8ae34ea63676979b0cf2ec
Shannon <Shannon@RLX-Tyler.rlx-ad.com>
2020-09-30 18:14:03 -0700

    Initial commit of project.

---
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,51 @@
+package cse360assignment02;
+
+public class AddingMachine {
+	/**
+	 * This is the total
+	 */
+  private int total;
+  
+  /**
+   * Creates a new AddingMachine with a total of 0
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  /**
+   * This method gets the total
+   * @return 0
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  /**
+   * This method adds
+   * @param value
+   */
+  public void add (int value) {
+  }
+
+  /**
+   * This method subtracts
+   * @param value
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * This method is the toString for the adding machine
+   * @return string
+   */
+  public String toString () {
+    return "";
+  }
+
+  /**
+   * This method clears
+   */
+  public void clear() {
+  }
+}
