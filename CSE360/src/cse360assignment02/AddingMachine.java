5dae115ea7eb0318016ebe4865cf590223534fca
Shannon <Shannon@RLX-Tyler.rlx-ad.com>
2020-09-30 18:31:19 -0700

    Added new functionality

--- CSE360/src/cse360assignment02/AddingMachine.java
+++ CSE360/src/cse360assignment02/AddingMachine.java
@@ -1,51 +1,66 @@
 package cse360assignment02;
-
+/**
+ * This is a machine that will add or subtract to/from a total
+ * @author Tyler
+ *
+ */
 public class AddingMachine {
 	/**
 	 * This is the total
 	 */
   private int total;
+  /**
+   * This keeps track of the history of the events
+   */
+  private String history;
   
   /**
    * Creates a new AddingMachine with a total of 0
    */
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+    history = total + "";
   }
   
   /**
    * This method gets the total
-   * @return 0
+   * @return total
    */
   public int getTotal () {
-    return 0;
+    return total;
   }
   
   /**
-   * This method adds
+   * This method adds the parameter to the total variable
    * @param value
    */
   public void add (int value) {
+	  total += value;
+	  history += " + " + value;
   }
 
   /**
-   * This method subtracts
+   * This method subtracts the parameter from the total variable
    * @param value
    */
   public void subtract (int value) {
+	  total -= value;
+	  history += " - " + value;
   }
 
   /**
-   * This method is the toString for the adding machine
-   * @return string
+   * This returns the history of the actions
+   * @return history
    */
   public String toString () {
-    return "";
+    return history;
   }
 
   /**
    * This method clears
    */
   public void clear() {
+	  total = 0;
+	  history = total + "";
   }
 }
